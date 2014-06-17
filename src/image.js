4b657ddd23d6eada13cbdbcaf1533cf6bcd9c67b
轩与 <wangwei.wang@alipay.com>
2014-06-17 20:25:21 +0800

    common js 化

--- src/image.js
+++ src/image.js
@@ -1,64 +1,44 @@
-;
-(function () {
-	/**
-	 *
-	 * AJ alipayJavascript
-	 * @namespace AJ
-	 * @author 凯达 <wb-zhenkd@alipay.com>
-	 * @version 1.0.0
-	 *
-	 * */
-	var AJ = window.AJ = window.AJ || {}
-	/**
-	 *
-	 * @namespace
-	 *
-	 * @memberof AJ
-	 * */
-	AJ.image = AJ.image || {};
-
-	/**
-	 * image转换base64编码方法
-	 *
-	 * @memberof AJ.image2base64
-	 * @param {!path} path - 图片地址（需要同域,项目目录）
-	 * @param {!function} callback - 返回数据
-	 *
-	 * @returns {string}
-	 *
-	 * @desc image转换base64编码方法
-	 *
-	 * @example
-	 * AJ.image.image2base64("abc.jpg",function(base64Data){
+var image = {};
+/**
+ * image转换base64编码方法
+ *
+ * @memberof AJ.toBase64
+ * @param {!path} path - 图片地址（需要同域,项目目录）
+ * @param {!function} callback - 返回数据
+ *
+ * @returns {string}
+ *
+ * @desc image转换base64编码方法
+ *
+ * @example
+ * AJ.image.image2base64("abc.jpg",function(base64Data){
      *  //data:image/png;base64.....
      * })
-	 */
-	var image2base64 = function (path, callback) {
+ */
+image.toBase64 = function (path, callback) {
+	var eleCanvas = document.createElement('canvas'),
+		ctx = eleCanvas.getContext('2d'),
+		img = new Image();
 
-		var eleCanvas = document.createElement('canvas'),
-			ctx = eleCanvas.getContext('2d'),
-			img = new Image();
+	img.onload = function () {
+		var dataValue = "";
 
-		img.onload = function () {
-			var dataValue = "";
-
-			eleCanvas.width = img.width;
-			eleCanvas.height = img.height;
-			ctx.drawImage(img, 0, 0, img.width, img.height);
-			try {
-				dataValue = eleCanvas.toDataURL();
-				eleCanvas = null;
-			} catch (e) {
-				console.warn(e);
-			}
-			callback(dataValue);
-		};
-		img.onerror = function () {
-			alert("图片无法加载，请检查对应地址的图片是否存在");
+		eleCanvas.width = img.width;
+		eleCanvas.height = img.height;
+		ctx.drawImage(img, 0, 0, img.width, img.height);
+		try {
+			dataValue = eleCanvas.toDataURL();
+			eleCanvas = null;
+		} catch (e) {
+			console.warn(e);
 		}
-
-		img.src = path;
-
+		callback(dataValue);
+	};
+	img.onerror = function () {
+		alert("图片无法加载，请检查对应地址的图片是否存在");
 	}
-	AJ.image.image2base64 = image2base64;
-})();
+	img.src = path;
+
+}
+
+module.exports = image;
