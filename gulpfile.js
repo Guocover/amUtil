6343cc6cfad3292926faf242e5519a6322bf8e7f
轩与 <wangwei.wang@alipay.com>
2014-06-18 14:04:47 +0800

    gulp 生成

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 var gulp = require('gulp'),
 	amTransportGulp = require('gulp-am-transport'),
-	concat = require('gulp-concat');
+	concat = require('gulp-concat'),
+	rename = require('gulp-rename'),
+	uglify = require('gulp-uglify');
 
 gulp.task('am-transport', function () {
 	gulp.src('./src/**/*.js')
-		.pipe(amTransportGulp({uglify:true}))
+		.pipe(amTransportGulp())
 		.pipe(concat('aj.js'))
+		.pipe(gulp.dest('./dist/'))
+		.pipe(uglify())
+		.pipe(rename({ suffix: '.min' }))
 		.pipe(gulp.dest('./dist/'));
 });
+
 
 // The default task (called when you run `gulp` from cli)
 gulp.task('default', ['am-transport']);
